e4a7477a64efc712349162652eecd77075452308
woodman <woodman@edetw.com>
2023-09-12 13:32:41 +0800

    XElement組XML的範例

--- UOF24/CDS/XML/Default.aspx.cs
+++ UOF24/CDS/XML/Default.aspx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Xml;
+using System.Xml.Linq;
 
 public partial class CDS_XML_Default : Ede.Uof.Utility.Page.BasePage
 {
@@ -19,6 +20,15 @@ public partial class CDS_XML_Default : Ede.Uof.Utility.Page.BasePage
         //  <Contact name='A03' Phone='V03' />
         //<ContactList>
 
+        XElement xe = new XElement("ContactList");
+
+        XElement xe1 = new XElement("Contact", new XAttribute("name", "A01"), new XAttribute("Phone", "V01"));
+        XElement xe2 = new XElement("Contact", new XAttribute("name", "A02"), new XAttribute("Phone", "V02"));
+        XElement xe3 = new XElement("Contact", new XAttribute("name", "A03"), new XAttribute("Phone", "V03"));
+
+        xe.Add(xe1,xe2,xe3);
+        txtXML.Text = xe.ToString();
+        return;
         XmlDocument xmlDoc = new XmlDocument();
         //<ContactList/>
         XmlElement ContactListElement = xmlDoc.CreateElement("ContactList");
@@ -33,7 +43,7 @@ public partial class CDS_XML_Default : Ede.Uof.Utility.Page.BasePage
         Contact02Element.SetAttribute("name", "Wang");
         Contact02Element.SetAttribute("Phone", "0987654321");
     
-
+        
         //  <Item id='A03' value='V03' ></Iteem>        //
         XmlElement Contact03Element = xmlDoc.CreateElement("Contact");
         Contact03Element.SetAttribute("name", "Mary");
@@ -52,9 +62,19 @@ public partial class CDS_XML_Default : Ede.Uof.Utility.Page.BasePage
     }
     protected void btnGetValue_Click(object sender, EventArgs e)
     {
+
+        XElement xe = XElement.Parse(txtXML.Text);
+
+        var node = (from xl in xe.Elements("Contact")
+                    where xl.Attribute("name").Value == txtID.Text
+                    select xl).FirstOrDefault();
+        txtValue.Text = node.Attribute("Phone").Value;
+        return;
         XmlDocument xmlDoc = new XmlDocument();
         xmlDoc.LoadXml(txtXML.Text);
-
+        txtValue.Text = xmlDoc.SelectSingleNode
+            ($"./FieldValue/Item[@id='{txtID.Text}']").
+            Attributes["value"].Value;
         //<FieldValue>
         //  <Item id='A01' value='V01' />
         //  <Item id='A02' value='V02' >InnerText</Item>
@@ -62,9 +82,5 @@ public partial class CDS_XML_Default : Ede.Uof.Utility.Page.BasePage
         //<FieldValue>
 
 
-
-        txtValue.Text = xmlDoc.SelectSingleNode
-            ($"./FieldValue/Item[@id='{txtID.Text}']").
-            Attributes["value"].Value;
     }
 }
